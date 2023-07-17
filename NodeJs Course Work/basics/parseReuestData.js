5013f2513aea5a12b32451a0864fd8cd1ef2bf90
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2023-07-17 19:31:21 -0400

    parese data v

---
+++ NodeJs Course Work/basics/parseReuestData.js
@@ -0,0 +1,53 @@
+const http = require('http');
+//Handles file system 
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+
+    if (url === '/'){
+
+        res.write('<html>');
+        res.write('<head><title>The Form manin Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">send</button></form></body>');
+        res.write('</html>');
+        return res.end
+    }
+    if (url === '/message' && req.method === 'POST') {
+        const body = []
+        //on help to listen to the event listener 
+        req.on('data', (chunk) => {
+            body.push(chunk);
+
+        });
+
+        //end will parse the end event listener
+        req.on('end', () => {
+            // the incoming data wil be splited into chunks and parsed as body 
+            const parseBody = Buffer.concat(body).toString();
+            console.log(parseBody);
+            const message = parseBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+
+        });
+
+
+        
+        res.statusCode = 302;
+        //this will redirect to the main page 
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    s.setHeader('Content-Type','text/html');
+    //res.write();
+    res.write('<html>');
+    res.write('<head><title>The First Project Page</title></head>');
+    res.write('<body><h1>The Page Work<h1> </body>')
+    res.write('</head>');
+    res.end();
+    //Once the resposne is ended we can't write a another response 
+});
+
+
+//listen function - to make the server run on the port 
+server.listen(3000);
