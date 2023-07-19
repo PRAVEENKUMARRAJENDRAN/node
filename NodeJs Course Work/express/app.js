c97cb9a25e9f84659272157f09422e8b26085712
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2023-07-19 18:53:01 -0400

    express js

--- NodeJs Course Work/express/app.js
+++ NodeJs Course Work/express/app.js
@@ -16,11 +16,17 @@ app.use((req, res, next) => {
    next(); //the following next call will execute the next middleware in the line 
 });
 
-//second middlewaRE
+//second middlewaRE wit the response 
 app.use((req, res, next) => {
     console.log("The Second Middleware");
+
+    //the send e=given by express to send the resposne
+    res.send('<h1>The response is send from the middleware</h1>')
 });
 //app willact as a requesthandler so we pass to the createServer
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
-server.listen(3000);
+//server.listen(3000);
+
+//the express way
+app.listen(3000);
