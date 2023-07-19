06f207b3cf09546fc0d1904c0529e01e556fc78c
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2023-07-19 18:41:48 -0400

    middleware

--- NodeJs Course Work/express/app.js
+++ NodeJs Course Work/express/app.js
@@ -7,6 +7,19 @@ const express = require('express');
 //All the express related helper will be inside the express function
 //so we save it inside the app module
 const app = express();
+
+//Middleware Section 
+//use function of express is used to set a middleware
+//three arguments needed for middleware
+app.use((req, res, next) => {
+    console.log('The First Middleware');
+   next(); //the following next call will execute the next middleware in the line 
+});
+
+//second middlewaRE
+app.use((req, res, next) => {
+    console.log("The Second Middleware");
+});
 //app willact as a requesthandler so we pass to the createServer
 const server = http.createServer(app);
 
