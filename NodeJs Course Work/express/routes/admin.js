e10f3439a207dd7d85e9c9742643fff93033a869
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2023-07-20 20:12:38 -0400

    routes

---
+++ NodeJs Course Work/express/routes/admin.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// The router will act as a mini express function 
+const router = express.Router();
+//get method 
+router.get('/add-product', (req, res, next) => {
+
+    res.send(
+        ''
+    )
+});
+
+// post method 
+router.post('/add-product', (req, res, next) => {
+
+    res.send(
+        ''
+    )
+});
+
+
+
+
+module.exports = router;
+
+
+
